b363486f2f14e7ba081d444ad0ea110a8f33fd59
Washington Gacho <washington.gacho@dcomp.ufs.br>
2021-11-26 09:12:57 -0300

    JS/hamburgueria

---
+++ JS/hamburgueria/script.js
@@ -0,0 +1,12 @@
+function calculatePrice() {
+  const inputs = document.querySelectorAll("input");
+
+  let price = 0;
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      price += parseFloat(inputs[i].value);
+    }
+  }
+  document.getElementById("result").innerHTML = String(price);
+}
+calculatePrice()
