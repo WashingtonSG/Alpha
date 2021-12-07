5e6d50ee2fc7661baafba51985d374d29024aebc
Washington Gacho <washington.gacho@dcomp.ufs.br>
2021-12-07 07:27:09 -0300

    JS/cars-object objects properties update

--- JS/cars-object/script.js
+++ JS/cars-object/script.js
@@ -10,40 +10,23 @@ function calculateVelocity(minVelocity, maxVelocity, skid) {
 function createCar (chance) {
   if (chance < 0.6) {
     const commonCar = new Object ();
-    commonCar.minVelocity = calculateRandom(110,130);
-    commonCar.maxVelocity = calculateRandom(180,200);
-    commonCar.skid = calculateRandom(3,4);
-
-    commonCar.velocity = calculateVelocity(
-      commonCar.minVelocity, 
-      commonCar.maxVelocity, 
-      commonCar.skid);
-
+    commonCar.minVelocity = {min: 110, max: 130};
+    commonCar.maxVelocity = {min: 180, max: 200};
+    commonCar.skid = {min: 3,max:4};
     return commonCar;  
 
   } else  if(chance > 0.6 && chance < 0.95){
     const sportCar = new Object ()
-    sportCar.minVelocity =  calculateRandom(125, 145);
-    sportCar.maxVelocity =  calculateRandom(195, 215);
-    sportCar.skid =  calculateRandom(2, 3);
-    sportCar.velocity = calculateVelocity(
-      sportCar.minVelocity, 
-      sportCar.maxVelocity, 
-      sportCar.skid);
-
+    sportCar.minVelocity =  {min: 125, max: 145};
+    sportCar.maxVelocity =  {min: 195, max: 215};
+    sportCar.skid =  {min: 2, max: 3};
     return sportCar;
   }
   else {
     const superSportCar = new Object ();
-    superSportCar.minVelocity =  calculateRandom(140, 160);
-    superSportCar.maxVelocity =  calculateRandom(210, 230);
-    superSportCar.skid =  calculateRandom(1, 1.75);
-    
-    superSportCar.velocity = calculateVelocity(
-      superSportCar.minVelocity, 
-      superSportCar.maxVelocity,
-      superSportCar.skid);
-
+    superSportCar.minVelocity =  {min:140, max: 160};
+    superSportCar.maxVelocity =  {min:210, max: 230};
+    superSportCar.skid =  {min: 1, max: 1.75};
     return superSportCar;
   }
 }
@@ -75,7 +58,25 @@ function race(laps) {
     juca  = createCar( Math.random() );
     edna  = createCar( Math.random() );
 
-    wins[greater([pedro.velocity, juca.velocity, edna.velocity])] +=1;
+    pedroVelocity = calculateVelocity(
+      calculateRandom(pedro.minVelocity.min, pedro.minVelocity.max),
+      calculateRandom(pedro.maxVelocity.min, pedro.maxVelocity.max),
+      calculateRandom(pedro.skid.min, pedro.skid.max)
+      );
+
+    jucaVelocity = calculateVelocity(
+      calculateRandom(juca.minVelocity.min, juca.minVelocity.max),
+      calculateRandom(juca.maxVelocity.min, juca.maxVelocity.max),
+      calculateRandom(juca.skid.min, juca.skid.max)
+      );
+
+    ednaVelocity = calculateVelocity(
+      calculateRandom(edna.minVelocity.min, edna.minVelocity.max),
+      calculateRandom(edna.maxVelocity.min, edna.maxVelocity.max),
+      calculateRandom(edna.skid.min, edna.skid.max)
+      );
+      
+    wins[ greater([pedroVelocity, jucaVelocity, ednaVelocity]) ] +=1;
 
   }
   return [greater(wins), wins[greater(wins)]]; 
@@ -87,18 +88,17 @@ function insertion() {
   const winnerLaps = race(laps)[1];
   const result = document.getElementById("result")
   result.innerHTML = "";
-  result.innerHTML = "Você ganhou:"
 
   if (winner == 0) {
-   result.innerHTML += '<span id="winner">Pedro com '
+   result.innerHTML += '<span id="winner">Pedro ganhou com '
   + winnerLaps + " voltas";
   } 
   else if(winner ==1) {
-    result.innerHTML += '<span id="winner">Juca com '
+    result.innerHTML += '<span id="winner">Juca ganhou com '
     + winnerLaps + " voltas";
   }
   else {
-    result.innerHTML += '<span id="winner">Edna com '
+    result.innerHTML += '<span id="winner">Edna ganhou com '
     + winnerLaps + " voltas";
   }
 }
